a82bcef919eb48a416ad91e5c5ae6635b45e7b75
CF12 <brianx0011@gmail.com>
2018-12-01 18:27:53 -0500

    Added server code

--- server/src/server.js
+++ server/src/server.js
@@ -1,16 +1,35 @@
+const fs = require('fs')
 const express = require('express')
-const RaspiCam = require('raspicam')
-
-
+const camera = require('raspberry-pi-camera-native')
 
 const PORT = process.env.PORT || 3001
 const app = express()
-const camera = new RaspiCam()
 
-camera.start()
+const cameraOptions = {
+  width: 1280,
+  height: 720,
+  fps: 2,
+  encoding: 'JPEG',
+  quality: 75
+}
+
+camera.start(cameraOptions)
+camera.on('frame', data => {
+  console.log(data)
+  writeImg(data)
+})
+
+let count = 0
+
+function writeImg (data) {
+  fs.writeFile(`./imgs/img${count}.jpeg`, data, 'binary', (err) => {
+    console.error(err)
+  })
+  count++
+}
 
 app.get('/video', (req, res) => {
-
+  res.send()
 })
 
 app.listen(PORT)
