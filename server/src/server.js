dc4bd47be382b810745953359e3a7115e9585da4
CF12 <brianx0011@gmail.com>
2018-12-01 19:46:26 -0500

    commit files

--- server/src/server.js
+++ server/src/server.js
@@ -1,37 +1,40 @@
 const fs = require('fs')
 const express = require('express')
 const camera = require('raspberry-pi-camera-native')
+const ffmpge = require('ffmpeg')
 
 const PORT = process.env.PORT || 3001
 const app = express()
 
 const cameraOptions = {
-  width: 1280,
-  height: 720,
+  width: 1920,
+  height: 1080,
   fps: 2,
   encoding: 'JPEG',
-  quality: 75
+  quality: 2
 }
 
 camera.start(cameraOptions)
+
 camera.on('frame', data => {
-  console.log(data)
-  writeImg(data)
+  
 })
 
-let count = 0
-
-function writeImg (data) {
-  fs.writeFile(`./imgs/img${count}.jpeg`, data, 'binary', (err) => {
-    console.error(err)
-  })
-  count++
-}
-
 app.get('/video', (req, res) => {
-  res.send()
+  camera.on('frame', data => {
+    console.log(data)
+
+    const head = {
+      'Content-Type': 'video/mp4',
+    }
+    
+    res.writeHead(206, head)
+
+    res.end(data, 'binary')
+  })
 })
 
 app.listen(PORT)
 console.log('Listening on port: ' + PORT)
+
 
